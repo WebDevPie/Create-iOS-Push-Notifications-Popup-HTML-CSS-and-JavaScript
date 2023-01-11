6b196da6d74850019d88b27342dc2f9f5115cb37
WebDevPie <nazarmarchyshyn@gmail.com>
2023-01-11 17:13:06 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    setTimeout(() => {
+        $(".push__box:nth-child(1)").slideDown(400);
+    }, 1000);
+    setTimeout(() => {
+        $(".push__box:nth-child(2)").slideDown(400);
+    }, 2000);
+    setTimeout(() => {
+        $(".push__box:nth-child(3)").slideDown(400);
+    }, 3000);
+});
